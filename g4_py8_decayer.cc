d9894aeda9630c28acc31b7915055e9859533df3
Robert Hatcher <rhatcher@fnal.gov>
2020-04-21 16:25:15 -0500

    integrate factory methods; whitespace cleanup

--- g4_py8_decayer.cc
+++ g4_py8_decayer.cc
@@ -27,6 +27,9 @@
 /// \file eventgenerator/pythia/decayer6/pythia6_decayer.cc
 /// \brief Main program of the pythia6Decayer example
 
+#include "G4PhysListFactoryAlt.hh"
+#include "G4PhysListRegistry.hh"
+
 #include "Py8DecayerPhysics.hh"
 
 #include "DetConstruction.hh"
@@ -40,14 +43,33 @@
 #include "Randomize.hh"
 #include "G4GeometryManager.hh"
 
-// int main(int argc,char** argv)
-int main()
+#include "G4PhysListFactoryAlt.hh"
+
+// allow ourselves to extend the short names for physics ctor addition/replace
+// along the same lines as EMX, EMY, etc
+#include "G4PhysListRegistry.hh"
+
+// allow ourselves to give the user extra info about available physics ctors
+#include "G4PhysicsConstructorFactory.hh"
+
+// forward declaration
+void PrintAvailable(G4int verb=1);
+
+int main(int argc,char** argv)
 {
+
+  // pick physics list
+  std::string physListName = "FTFP_BERT+PY8DK";
+
+  for ( G4int i=1; i<argc; i=i+2 ) {
+    G4String g4argv(argv[i]);  // convert only once
+    if ( g4argv == "-p" ) physListName = argv[i+1];
+  }
 
    // Choose the Random engine
    //
    G4Random::setTheEngine(new CLHEP::RanecuEngine);
-  
+
    // Construct the default run manager
    //
    G4RunManager * runManager = new G4RunManager();
@@ -61,8 +83,35 @@ int main()
    //
    // Physics
    //
-   G4VModularPhysicsList* physicsList = new FTFP_BERT();
-   physicsList->RegisterPhysics(new Py8DecayerPhysics());
+   //G4VModularPhysicsList* physicsList = new FTFP_BERT();
+   //physicsList->RegisterPhysics(new Py8DecayerPhysics());
+
+   g4alt::G4PhysListFactory factory;
+   G4VModularPhysicsList* physicsList = nullptr;
+   factory.SetDefaultReferencePhysList("NO_DEFAULT_PHYSLIST");
+
+   // set a short name for the plugin
+   G4PhysListRegistry* plreg = G4PhysListRegistry::Instance();
+   plreg->AddPhysicsExtension("PY8DK","Py8DecayerPhysics");
+
+   physicsList = factory.GetReferencePhysList(physListName);
+
+   if ( ! physicsList ) {
+     int verbosity = 1;
+     PrintAvailable(verbosity);
+
+     // if we can't get what the user asked for...
+     //    don't go on to use something else, that's confusing
+     G4ExceptionDescription ED;
+     ED << "The factory for the physicslist ["
+        << physListName
+        << "] does not exist!"
+        << G4endl;
+     G4Exception("extensibleFactory",
+                 "extensibleFactory001", FatalException, ED);
+     exit(42);
+   }
+
    runManager->SetUserInitialization(physicsList);
    //
    // Set user action classes, e.g. prim.generator (tau- gun), etc.
@@ -74,26 +123,26 @@ int main()
 
    // NOTE: this component derives from G4UserTrackingAction
    //       and serves largely as a "tester", i.e. to make sure
-   //       various secondaries resulting from decays, and 
+   //       various secondaries resulting from decays, and
    //       all subsequent particles are all processed sensibly
    //
    //       it's quite verbose, so use it only if really needed
    //
 // ---> if needs be --->   runManager->SetUserAction( new TrkAction() );
-   
+
    // Run initialization
    //
    runManager->InitializeGeometry();
-   runManager->InitializePhysics();      
+   runManager->InitializePhysics();
    runManager->Initialize();
    //
    runManager->ConfirmBeamOnCondition();
-   runManager->RunInitialization(); // this is part of BeamOn 
-                                    // and needs be done (at least) to set GeomClosed status 
+   runManager->RunInitialization(); // this is part of BeamOn
+                                    // and needs be done (at least) to set GeomClosed status
 
    // Run event loop
-   // 
-   for (G4int iev=0; iev<5; ++iev) 
+   //
+   for (G4int iev=0; iev<5; ++iev)
    {
       runManager->ProcessOneEvent( iev );
    }
@@ -110,4 +159,23 @@ int main()
 
   return 0;
 
+}
+
+void PrintAvailable(G4int verbosity) {
+  G4cout << G4endl;
+  G4cout << "extensibleFactory: here are the available physics lists:"
+         << G4endl;
+  g4alt::G4PhysListFactory factory;
+  factory.PrintAvailablePhysLists();
+
+  // if user asked for extra verbosity then print physics ctors as well
+  if ( verbosity > 1 ) {
+    G4cout << G4endl;
+    G4cout << "extensibleFactory: "
+           << "here are the available physics ctors that can be added:"
+           << G4endl;
+    G4PhysicsConstructorRegistry* g4pctorFactory =
+      G4PhysicsConstructorRegistry::Instance();
+    g4pctorFactory->PrintAvailablePhysicsConstructors();
+  }
 }
